ac8a3f389e84f1e9f1ef3854b504b672b12305cb
Ding <ding.chen@cloud-interactive.com>
2021-04-16 23:08:57 +0800

    Modify button component type

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -5,33 +5,26 @@ type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
   label: string | React.ReactNode
   className?: string
   shape?: 'rounded' | 'square' | 'circle'
-  click?: () => void
 }
 
 const Button: React.ForwardRefRenderFunction<HTMLButtonElement, ButtonProps> = (
   props: ButtonProps,
   ref
 ) => {
-  const { label, className = '', click, type = 'button', shape = 'rounded' } = props
-
-  const handleClick = (): void => {
-    if (click) {
-      click()
-    }
-  }
+  const { label, className = '', type = 'button', shape = 'rounded', ...restProps } = props
 
   return (
     <button
       ref={ref}
       type={type}
       className={className}
-      onClick={handleClick}
       css={[
         tw`border-2 border-solid border-primary text-dark-gray-1 focus:(outline-none bg-primary text-gray-100 border-transparent) hover:(outline-none bg-primary text-gray-100 border-transparent)`,
         shape === 'rounded' && tw`py-2 px-10 rounded-3xl `,
         shape === 'circle' && tw`rounded-full w-10 h-10`,
         shape === 'square' && tw`py-2 px-10`
       ]}
+      {...restProps}
     >
       {label}
     </button>
