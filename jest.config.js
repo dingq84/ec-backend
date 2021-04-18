968f6cfe9298fd43f3ae492dacaa56b6f94a9264
Ding <ding.chen@cloud-interactive.com>
2021-04-18 14:47:31 +0800

    Modify jest path config

--- jest.config.js
+++ jest.config.js
@@ -72,11 +72,12 @@ module.exports = {
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
   moduleNameMapper: {
     '@/pages/(.*)': '<rootDir>/pages/$1',
-    '@/components/(.*)': '<rootDir>/src/components/$1',
-    '@/layouts/(.*)': '<rootDir>/src/layouts/$1',
-    '@/hooks/(.*)': '<rootDir>/src/hooks/$1',
-    '@/types/(.*)': '<rootDir>/src/types/$1',
-    '@/utils/(.*)': '<rootDir>/src/utils/$1'
+    '@/(.*)/(.*)': '<rootDir>/src/$1/$2'
+    // '@/components/(.*)': '<rootDir>/src/components/$1',
+    // '@/layouts/(.*)': '<rootDir>/src/layouts/$1',
+    // '@/hooks/(.*)': '<rootDir>/src/hooks/$1',
+    // '@/types/(.*)': '<rootDir>/src/types/$1',
+    // '@/utils/(.*)': '<rootDir>/src/utils/$1'
   },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
