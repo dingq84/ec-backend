fbab4245710b5acbf70a24b70c545bb12b40f939
Ding <ding.chen@cloud-interactive.com>
2021-08-31 23:04:24 +0800

    Fixed the import path

--- packages/core/src/admin/domains/useCases/repositories-interfaces/IAccount.ts
+++ packages/core/src/admin/domains/useCases/repositories-interfaces/IAccount.ts
@@ -1,6 +1,6 @@
 import { IErrorDTO } from '@/common/domains/dto/ErrorDTO'
 import { Either } from 'fp-ts/lib/Either'
-import { IAccountDTO } from '../../dto/AccountDTO'
+import { IAccountDTO } from '@/admin/domains/dto/AccountDTO'
 
 export interface IAccountRepository {
   updatePassword(parameter: IAccountDTO): Promise<Either<IErrorDTO, void>>
