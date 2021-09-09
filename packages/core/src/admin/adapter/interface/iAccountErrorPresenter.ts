9d5fc9fcf0d401e01d888e147154c219965e6350
Ding <ding.chen@cloud-interactive.com>
2021-09-09 10:19:04 +0800

    Remove unused code

--- packages/core/src/admin/adapter/interface/iAccountErrorPresenter.ts
+++ packages/core/src/admin/adapter/interface/iAccountErrorPresenter.ts
@@ -1,8 +1,6 @@
 import { Either } from 'fp-ts/lib/Either'
 
-import { IGetMeOutPort } from '@/auth/application/interface/iGetMeUseCase'
 import { ILoginOutputPort } from '@/auth/application/interface/iLoginUseCase'
-import { IMeEntity } from '@/auth/domain/interface/iMe'
 import { ITokenEntity } from '@/auth/domain/interface/iToken'
 import { IErrorInputPort, IErrorOutputPort } from '@/common/application/interface/iErrorUseCase'
 
